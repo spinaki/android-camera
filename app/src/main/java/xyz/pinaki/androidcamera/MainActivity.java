8973b5ff59c5c16f8b0dab9382b9b1f7ce359919
Pinaki Sinha <spinaki@gmail.com>
2017-02-28 13:05:49 -0800

    added shutter click handler. still npe

--- app/src/main/java/xyz/pinaki/androidcamera/MainActivity.java
+++ app/src/main/java/xyz/pinaki/androidcamera/MainActivity.java
@@ -1,7 +1,8 @@
 package xyz.pinaki.androidcamera;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,9 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final CameraFragment cameraFragment = CameraFragment.newInstance();
         if (null == savedInstanceState) {
             getSupportFragmentManager().beginTransaction().replace(
-                    R.id.container, CameraFragment.newInstance()).commit();
+                    R.id.container, cameraFragment).commit();
         }
+        View shutterIcon = findViewById(R.id.shutter);
+        shutterIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cameraFragment.takePicture();
+            }
+        });
     }
 }
