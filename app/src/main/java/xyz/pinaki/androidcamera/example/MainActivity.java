7794d6809d4c90313def867bb58c6abc0bfc6f8f
Pinaki Sinha <spinaki@gmail.com>
2017-03-08 12:55:57 -0800

    CameraManager api start

--- app/src/main/java/xyz/pinaki/androidcamera/example/MainActivity.java
+++ app/src/main/java/xyz/pinaki/androidcamera/example/MainActivity.java
@@ -3,7 +3,7 @@ package xyz.pinaki.androidcamera.example;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import xyz.pinaki.androidcamera.Camera;
+import xyz.pinaki.androidcamera.CameraManager;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -11,10 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (null == savedInstanceState) {
-            Camera camera = new Camera();
-            camera.launch(this, R.id.container);
-//            getSupportFragmentManager().beginTransaction().replace(
-//                    R.id.container, CameraFragment.newInstance(), "CameraFragment").commit();
+            CameraManager.getInstance().launch(this, R.id.container);
         }
     }
 }
