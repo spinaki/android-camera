de7bc48f59b1df329e9e67a57dd027b77c72e78c
Pinaki Sinha <spinaki@gmail.com>
2017-10-02 05:41:29 -0700

    save images

--- app/src/main/java/xyz/pinaki/androidcamera/example/MainActivity.java
+++ app/src/main/java/xyz/pinaki/androidcamera/example/MainActivity.java
@@ -4,10 +4,17 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import xyz.pinaki.android.camera.CameraController;
 
@@ -46,6 +53,32 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onBitmapProcessed(Bitmap bitmap) {
                 Log.i(TAG, "onBitmapProcessed dimensions" + bitmap.getWidth() + ", " + bitmap.getHeight());
+                // save the image for testing
+                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                String imageFileName = "JPEG_" + timeStamp + "_";
+                File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+                FileOutputStream fos = null;
+
+
+                try {
+                    File image = File.createTempFile(
+                            imageFileName,  /* prefix */
+                            ".jpg",         /* suffix */
+                            storageDir      /* directory */
+                    );
+                    Log.i("PIANKI-STORAGE", image.getAbsolutePath());
+                    fos = new FileOutputStream(image);
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        fos.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
             }
         };
         CameraController.getInstance().launch(this, R.id.container, callback);
